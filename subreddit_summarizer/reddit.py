6dd36836181bb3047f0ef17bf8542fd3ceab61de
Sándor Uszkai <uszkaisanyi@gmail.com>
2018-12-12 21:13:29 +0100

    Add delete subreddit feature

--- subreddit_summarizer/reddit.py
+++ subreddit_summarizer/reddit.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import pymongo
 import json
 import requests
-import pytz 
+import pytz
 
 bp = Blueprint('reddit', __name__, url_prefix='/reddit')
 
@@ -17,9 +17,10 @@ def time_from_int(unix_time):
     old_timezone = pytz.timezone("UTC")
     new_timezone = pytz.timezone("Europe/Budapest")
     # returns datetime in the new timezone
-    timestamp_in_new_timezone = old_timezone.localize(timestamp).astimezone(new_timezone) 
+    timestamp_in_new_timezone = old_timezone.localize(
+        timestamp).astimezone(new_timezone)
     return timestamp_in_new_timezone.strftime('%Y-%m-%d %H:%M')
-    
+
 
 def new_subreddit(response, subreddit):
     conn = pymongo.MongoClient()    # connect to localhost
@@ -34,7 +35,7 @@ def new_subreddit(response, subreddit):
                          '$push': {'subreddits': subreddit}})
         for item in response['data']['children']:
             item['_id'] = item['data']['id']
-            item['data']['created'] =  time_from_int(item['data']['created'])
+            item['data']['created'] = time_from_int(item['data']['created'])
             users.update_one({'username': session['username']}, {
                              '$push': {'posts': item}})
         context = {
@@ -51,22 +52,21 @@ def update_all_posts():
     db = conn['redditclient']    # select database
     users = db['users']   # select users collection
     cursor_subred = users.find_one(
-        {'username': session['username']},{ '_id': 0, 'subreddits': 1})
+        {'username': session['username']}, {'_id': 0, 'subreddits': 1})
     if len(cursor_subred['subreddits']) > 0:
-        users.update_one({'username': session['username']}, {'$unset': { 'posts': 1 }})
+        users.update_one({'username': session['username']}, {
+                         '$unset': {'posts': 1}})
     for subred in cursor_subred['subreddits']:
-        current_app.logger.info(subred)
         # User agent
         url = "http://www.reddit.com/r/{}/.json".format(subred)
         headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         response = requests.get(url, headers=headers).json()
         for item in response['data']['children']:
             item['_id'] = item['data']['id']
-            item['data']['created'] =  time_from_int(item['data']['created'])
+            item['data']['created'] = time_from_int(item['data']['created'])
             users.update_one({'username': session['username']}, {
                              '$push': {'posts': item}}, upsert=True)
-
 
 
 def get_post(subreddit):
@@ -98,7 +98,7 @@ def get_reddit():
                                 '_id': 0, 'posts': 1})
         # Sort posts by reddit score
         sorted_by_score = sorted(cursor['posts'], key=lambda i: i['data']
-                    ['score'], reverse=True)
+                                 ['score'], reverse=True)
         cursor['posts'] = sorted_by_score
         return render_template('reddit.html', username=session['username'], **cursor)
 
@@ -112,7 +112,6 @@ def list_my_subreddits():
         actual_user_subreddits = users.find_one(
             {'username': session['username']}, {'_id': 0, 'subreddits': 1})
         warning = None
-        current_app.logger.info(actual_user_subreddits)
         if actual_user_subreddits['subreddits'] == []:
             warning = "There are no subreddits yet."
         # TODO: Serialize actual_user_subreddits to python object
@@ -129,3 +128,12 @@ def add_subreddit():
     context = get_post(new_subreddit)
     return render_template('reddit.html', **context)
 
+
+@bp.route('/delete_subreddit/<subred_name>', methods=['POST', 'GET'])
+def delete_subreddit(subred_name):
+    conn = pymongo.MongoClient()    # connect to localhost
+    db = conn['redditclient']    # select database
+    users = db['users']   # select users collection
+    users.update_one({'username': session['username']}, {
+                         '$pull': {'subreddits': subred_name}})
+    return redirect(url_for('reddit.list_my_subreddits'))
